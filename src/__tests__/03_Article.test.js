1d1ed76fc086f5ba71896b4c42918ee546569421
Stephen Fang <safang99@gmail.com>
2021-03-11 15:55:25 -0500

    updated script for pc and mac

--- src/__tests__/03_Article.test.js
+++ src/__tests__/03_Article.test.js
@@ -1,69 +1,68 @@
-import Comment from '../Comment.js'
-import Article from '../Article.js'
-import Author from '../Author.js'
+import Comment from "../Comment.js"
+import Article from "../Article.js"
+import Author from "../Author.js"
 
-describe('an article', () => {
+describe("an article", () => {
   let author
   let article
   let title
   let body
 
   beforeEach(() => {
-    author = new Author('Roald', 'Dahl')
-    body = 'Something smart to say'
-    title = 'Dan screwed up the test suite'
+    author = new Author("Roald", "Dahl")
+    body = "Something smart to say"
+    title = "Dan screwed up the test suite"
     article = new Article(author, title, body)
   })
 
-  it('has an author', () => {
+  it("has an author", () => {
     expect(article.author).toEqual(author)
   })
 
-  it('has a title', () => {
+  it("has a title", () => {
     expect(article.title).toEqual(title)
   })
 
-  it('has a body', () => {
+  it("has a body", () => {
     expect(article.body).toEqual(body)
   })
 
   describe("#wordCount", () => {
-    it('returns the number of words in the articles body', () => {
+    it("returns the number of words in the articles body", () => {
       expect(article.wordCount()).toEqual(4)
     })
   })
 
   describe("#description", () => {
-    it('returns description that includes the author name', () => {
+    it("returns description that includes the author name", () => {
       expect(article.description()).toMatch(author.fullName())
     })
-  
-    it('returns description that includes the title', () => {
+
+    it("returns description that includes the title", () => {
       expect(article.description()).toMatch(article.title)
     })
   })
 
-
-  it('starts with an empty list of comments', () => {
+  it("starts with an empty list of comments", () => {
     expect(article.comments.length).toEqual(0)
   })
 
   describe("#addComment", () => {
-    it('adds a comment to the article\s list of comments', () => {
-      const comment = new Comment('I am so smart', author)
+    it("adds a comment to the articles list of comments", () => {
+      const comment = new Comment("I am so smart", author)
       article.addComment(comment)
       expect(article.comments.length).toEqual(1)
       expect(article.comments).toEqual([comment])
     })
   })
 
-  describe('#formattedString', () => {
+  describe("#formattedString", () => {
     beforeEach(() => {
-      let comment = new Comment('Awesome!', author)
+      let comment = new Comment("Awesome!", author)
       article.addComment(comment)
     })
 
-    it('includes the title', () => {
+    it("includes the title", () => {
       expect(article.formattedString()).toMatch(article.title)
     })
 
@@ -71,23 +70,23 @@ describe('an article', () => {
       expect(article.formattedString()).toMatch(article.author.fullName())
     })
 
-    it('includes the number of comments', () => {
+    it("includes the number of comments", () => {
       expect(article.formattedString()).toMatch(
-        article.commentCount().toString()
+        article.comments.length.toString()
       )
     })
 
-    it('includes the body', () => {
+    it("includes the body", () => {
       expect(article.formattedString()).toMatch(article.body)
     })
   })
 
-  describe('#commentsMentioning', () => {
-    it('returns a list of comments whose body contains a provided phrase', () => {
-      article.addComment(new Comment('Spaceforce!', author))
-      article.addComment(new Comment('Regular Comment', author))
-      article.addComment(new Comment('I want to join spaceforce', author))
-      expect(article.commentsMentioning('spaceforce').length).toEqual(2)
+  describe("#commentsMentioning", () => {
+    it("returns a list of comments whose body contains a provided phrase", () => {
+      article.addComment(new Comment("Spaceforce!", author))
+      article.addComment(new Comment("Regular Comment", author))
+      article.addComment(new Comment("I want to join spaceforce", author))
+      expect(article.commentsMentioning("spaceforce").length).toEqual(2)
     })
   })
 })
